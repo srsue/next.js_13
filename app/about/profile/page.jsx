1950bd0c9358f593259645aa15e02a13c60bdcab
vmassue <suematsu.tohshin@gmail.com>
2023-06-08 14:49:02 +0900

    make srsue portfolio site first

--- app/about/profile/page.jsx
+++ app/about/profile/page.jsx
@@ -1,7 +1,11 @@
-const About = () => {
+export const metadata = {
+  title: 'About srsue portfolio site'
+}
+
+const Profile = () => {
   return (
     <div>
-      <h1>About srsue Portfolio site</h1>
+      <h1>srsues Profile is</h1>
       <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam earum eveniet rem nobis consequatur quia? Vel eligendi quos saepe quas assumenda quasi sint doloribus blanditiis ipsum accusantium aliquid neque laborum libero corporis, et quam. Culpa quisquam quae id quo debitis dolore, sunt iusto. Possimus explicabo delectus maxime, provident dolor tempora?
       </p>
@@ -9,4 +13,4 @@ const About = () => {
   )
 };
 
-export default About;
+export default Profile;
--- app/page.jsx
+++ app/page.jsx
@@ -1,25 +1,15 @@
-import Image from 'next/image'
-import Link from 'next/link';
+import Link from 'next/link'
 
-export default function Home() {
+const Home = () => {
   return (
-    <header>
-      <nav className="p-2">
-        <ul className="flex items-center space-x-2">
-          <li>
-            <img src="/vercel.svg" className="w-32" />
-          </li>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/about">About</Link>
-          </li>
-          <li>
-            <Link href="/header">Header</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <h1>Wellcome to srsue Portfolio</h1>
+      <ul>
+        <li><Link href='/'>Home</Link></li>
+        <li><Link href='/about'>About</Link></li>
+        <li><Link href='/about/profile'>Profile</Link></li>
+      </ul>
+    </>
   )
 }
+export default Home;
